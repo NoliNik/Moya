30ec07dc57c429c22add978ec299edc40cabf748
Mikalai Palto <npalto2139@yandex.ru>
2019-11-13 23:25:49 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 let package = Package(
     name: "Moya",
     products: [
-        .library(name: "Moya", targets: ["Moya"]),
+        .library(name: "Moya", targets: ["Moya"])
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0")),
+        .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0"))
     ],
     targets: [
          .target(
